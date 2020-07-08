7e4eeb5349ebcab907f03f60173da0ff44e04ee6
Alexey <dyakonov128@gmail.com>
2020-07-08 21:00:30 +0500

    first

---
+++ app/src/main/java/com/example/testsummer/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.testsummer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private static final String[] PERMISSION = {
+        Manifest.permission.READ_EXTERNAL_STORAGE
+    };
+
+    private static final int REQUEST_PERMISSIONS = 12345;
+
+    private static final int PERMISSION_COUNT = 1;
+
+    @SuppressLint("NewAPI")
+    private boolean arePermissionsDenied(){
+        for (int i = 0; i < PERMISSION_COUNT; i++){
+            if (checkSelfPermission(PERMISSION[i]) != PackageManager.PERMISSION_GRANTED) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
