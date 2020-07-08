c338b35a7ee1c635937ded57cd3811b0f824147d
Alexey <dyakonov128@gmail.com>
2020-07-09 02:04:47 +0500

    "Add working play button!"

--- app/src/main/java/com/example/testsummer/activity_play.java
+++ app/src/main/java/com/example/testsummer/activity_play.java
@@ -2,25 +2,32 @@ package com.example.testsummer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+
+import java.util.ArrayList;
 
 public class activity_play extends AppCompatActivity {
 
-    private ImageButton mainImageButton;
-    private ImageButton settingImageButton;
+    ImageButton mainImageButton, settingImageButton, playImageButton;
+    private ArrayList listSong = new ArrayList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        listSong.add("one");
+        listSong.add("two");
+        listSong.add("three");
+        listSong.add("four");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
 
         mainImageButton = findViewById(R.id.mainImageButton);
         settingImageButton = findViewById(R.id.settingImageButton);
+        playImageButton = findViewById(R.id.playImageButton);
+
         mainImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -34,6 +41,22 @@ public class activity_play extends AppCompatActivity {
                 setContentView(R.layout.activity_acticity_setting);
             }
         });
+
+        final MediaPlayer mediaPlayer = MediaPlayer.create(activity_play.this, R.raw.one);
+
+        playImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying()){
+                    mediaPlayer.pause();
+                }
+                else{
+                    mediaPlayer.start();
+                }
+            }
+        });
+
+
 
 
     }
