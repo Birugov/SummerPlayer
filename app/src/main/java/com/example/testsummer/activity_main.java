5ab214abe1448051d9b7b555f65af422121d1b16
KoaLa1101 <rproba@list.ru>
2020-07-09 10:13:51 +0300

    // some part of playing songs

--- app/src/main/java/com/example/testsummer/activity_main.java
+++ app/src/main/java/com/example/testsummer/activity_main.java
@@ -5,8 +5,10 @@ import android.annotation.SuppressLint;
 import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
@@ -23,6 +25,7 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.io.IOException;
 import java.net.URI;
 import java.util.ArrayList;
 
@@ -35,18 +38,26 @@ public class activity_main extends AppCompatActivity {
     private static final int REQUEST_PERMISSIONS = 12345;
     private static final int PERMISSION_COUNT = 1;
 
+    boolean prepared = false;
+    boolean started = false;
+
     private Button toPlay;
     MediaPlayer mediaPlayer;
     private ListView listOfSongs;
+    private String stream;
+    private String title;
 
     ArrayList<String> arrayList;
     ArrayAdapter<String> adapter;
+    ArrayList<String> arrayStreams;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
         setContentView(R.layout.activity_main);
         toPlay = findViewById(R.id.toPlay);
         toPlay.setOnClickListener(new View.OnClickListener() {
@@ -72,7 +83,7 @@ public class activity_main extends AppCompatActivity {
         }
     }
 
-    public void getMusic(){
+    public void getMusic() throws IOException {
         ContentResolver contentResolver = getContentResolver();
         Uri uriSong = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
         Cursor songCursor = contentResolver.query(uriSong, null, null,null, null);
@@ -80,14 +91,21 @@ public class activity_main extends AppCompatActivity {
         if(songCursor !=null && songCursor.moveToFirst()){
             int songTitle = songCursor.getColumnIndex(MediaStore.Audio.Media.TITLE);
             int songArtist = songCursor.getColumnIndex(MediaStore.Audio.Media.ARTIST);
+            int songLocation = songCursor.getColumnIndex(MediaStore.Audio.Media.DATA);
 
             do{
                 String currentTitle = songCursor.getString(songTitle);
+                title = currentTitle;
                 String currentArtist = songCursor.getString(songArtist);
-                arrayList.add(currentTitle + "/n" + currentArtist);
+                String currentLocation = songCursor.getString(songLocation);
+                stream = currentLocation;
+
+                arrayList.add("Title: " + currentTitle + "\n"
+                        + "Artist: " + currentArtist/* + "/n" + "Location: " + currentLocation*/);
             } while (songCursor.moveToNext());
         }
     }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
@@ -113,26 +131,54 @@ public class activity_main extends AppCompatActivity {
     public void doStuff(){
         listOfSongs = findViewById(R.id.listOfSongs);
         arrayList = new ArrayList<>();
-        getMusic();
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+        try {
+            getMusic();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, arrayList);
         listOfSongs.setAdapter(adapter);
 
         listOfSongs.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                //some actions with song
+                new PLayerTask().execute(stream);
+
+
             }
         });
 
     }
 
-    @SuppressLint("NewAPI")
-    private boolean arePermissionsDenied(){
-        for (int i = 0; i < PERMISSION_COUNT; i++){
-            if (checkSelfPermission(PERMISSION[i]) != PackageManager.PERMISSION_GRANTED) {
-                return true;
+    private class PLayerTask extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... strings) {
+            try {
+                mediaPlayer.setDataSource(stream);
+                mediaPlayer.prepare();
+                prepared = true;
+            } catch (IOException e) {
+                e.printStackTrace();
             }
+
+            return prepared;
         }
-        return false;
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            super.onPostExecute(aBoolean);
+            mediaPlayer.start();
+            Toast.makeText(activity_main.this, "Playing..  " + title, Toast.LENGTH_SHORT).show();
+        }
     }
+
+      /* @SuppressLint("NewAPI")
+     private boolean arePermissionsDenied(){
+         for (int i = 0; i < PERMISSION_COUNT; i++){
+             if (checkSelfPermission(PERMISSION[i]) != PackageManager.PERMISSION_GRANTED) {
+                 return true;
+             }
+         }
+         return false;
+     }*/
 }
