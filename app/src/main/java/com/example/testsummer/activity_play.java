28d6e8ea4826c5657cde589e2c06953fcaf1173f
Alexey <dyakonov128@gmail.com>
2020-07-09 13:33:28 +0500

    "Add working left and right buttons!"

--- app/src/main/java/com/example/testsummer/activity_play.java
+++ app/src/main/java/com/example/testsummer/activity_play.java
@@ -2,31 +2,68 @@ package com.example.testsummer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.AudioManager;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
-
 import java.util.ArrayList;
 
 public class activity_play extends AppCompatActivity {
 
-    ImageButton mainImageButton, settingImageButton, playImageButton;
-    private ArrayList listSong = new ArrayList();
+    ImageButton mainImageButton, settingImageButton, playImageButton, leftImageButton, rightImageButton;
+    int currentSong = 0;
+    private ArrayList<Integer> listSong = new ArrayList<>();
+    MediaPlayer mediaPlayer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        listSong.add("one");
-        listSong.add("two");
-        listSong.add("three");
-        listSong.add("four");
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer = MediaPlayer.create(activity_play.this, R.raw.three);
+        mediaPlayer.start();
+
+        //тест песенками
+        listSong.add(R.raw.one);
+        listSong.add(R.raw.two);
+        listSong.add(R.raw.three);
+        listSong.add(R.raw.four);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
+        currentSong = 2;
 
         mainImageButton = findViewById(R.id.mainImageButton);
         settingImageButton = findViewById(R.id.settingImageButton);
         playImageButton = findViewById(R.id.playImageButton);
+        leftImageButton = findViewById(R.id.leftImageButton);
+        rightImageButton = findViewById(R.id.rightImageButton);
+
+        leftImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentSong > 0){
+                    mediaPlayer.stop();
+                    mediaPlayer= MediaPlayer.create(activity_play.this, listSong.get(currentSong - 1));
+                    mediaPlayer.start();
+                    currentSong--;
+                }
+            }
+        });
+
+        rightImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentSong < listSong.size() - 1){
+                    currentSong++;
+                    mediaPlayer.stop();
+                    mediaPlayer = MediaPlayer.create(activity_play.this, listSong.get(currentSong));
+                    mediaPlayer.start();
+                }
+            }
+        });
+
+
 
         mainImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -42,8 +79,6 @@ public class activity_play extends AppCompatActivity {
             }
         });
 
-        final MediaPlayer mediaPlayer = MediaPlayer.create(activity_play.this, R.raw.one);
-
         playImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -56,12 +91,7 @@ public class activity_play extends AppCompatActivity {
             }
         });
 
-
-
-
     }
-
-
 
 
 }
